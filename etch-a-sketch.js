2ae4feb174378c7cc2f57d2374ce0c0f23e53a6a
Cialux <Garycialuxkellogg@gmail.com>
2022-07-14 14:47:57 +0200

    Add flexability to set grid size by height and width

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,24 @@
+
+let height = prompt("Height");
+let width = prompt("Width");
+
+if (width > 60) {
+    alert("too wide, set to 60");
+    width = 60;
+}
+
+let containerHeight = (height * 31)+"px";
+let containerWidth = (width * 31)+"px";
+
 const container = document.getElementById("squareContainer");
+
+container.style.width = containerWidth;
+container.style.height = containerHeight;
+
 console.log(container);
-for (let i = 0; i < 16; ++i) {
-    for (let i = 0; i < 16; ++i) {
+
+for (let i = 0; i < height; ++i) {
+    for (let i = 0; i < width; ++i) {
         let cell = document.createElement("div");
         cell.addEventListener('mouseover', 
         e => e.target.classList.add('my-colour-class')
